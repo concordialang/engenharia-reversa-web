bca9ff3552f67e7325db2beb4b40977e51830b65
Danilo Miranda <danilostriotto@hotmail.com>
2020-05-07 00:10:52 -0300

    refatoração: criada classe crawler

---
+++ src/app/Crawler.ts
@@ -0,0 +1,105 @@
+import { GraphStorage } from "./graph/GraphStorage";
+import { Graph } from "./graph/Graph";
+import Mutex from "idb-mutex";
+
+//classe deve ser refatorada
+export class Crawler {
+
+    private graphStorage : GraphStorage;
+    //abstrair mutex em classe
+    private mutex : Mutex;
+
+    //aux variables
+    private closeWindow = false;
+
+    constructor( graphStorage : GraphStorage, mutex : Mutex ){
+        this.graphStorage = graphStorage;
+        this.mutex = mutex;
+    }
+
+    public crawl(){
+        const pageUrl : URL = new URL(window.location.href);
+        this.addUrlToGraph(pageUrl);
+        const links : HTMLCollectionOf<HTMLAnchorElement> = this.searchForLinks();
+        for (let i : number = 0; i < links.length; i++) {
+
+            const foundUrl : URL = new URL(links[i].href);
+            this.addUrlToGraph(foundUrl);
+            this.addUrlsLinkToGraph(pageUrl,foundUrl);
+            if(this.sameHostname(foundUrl,pageUrl) && !this.wasUrlAlreadyCrawled(foundUrl)){
+
+                this.setUrlAsCrawled(new URL(links[i].href));
+                chrome.runtime.sendMessage({ acao: "abrir-janela", url: links[i].href });
+                
+            }
+        }
+        this.closeWindow = true;
+    }
+
+    private searchForLinks() : HTMLCollectionOf<HTMLAnchorElement>{
+        return document.getElementsByTagName('a');
+    }
+
+    //refatorar função
+    private addUrlToGraph(url : URL) : void{
+        //mutex deveria ficar dentro de GraphStorage ou em Crawler ?
+        this.mutex.lock().then(() => {
+            let graph : Graph = this.graphStorage.get("grafo");
+            graph.addNode(url.toString());
+            this.graphStorage.save("grafo", graph);
+            return this.mutex.unlock();
+        }).then(() => {
+            if(this.closeWindow === true) window.close();
+        });
+    }
+
+    //refatorar função
+    private addUrlsLinkToGraph(urlFrom : URL,urlTo : URL) : void{
+        this.mutex.lock().then(() => {
+            let graph : Graph = this.graphStorage.get("grafo");
+            graph.addEdge(urlFrom.toString(),urlTo.toString());
+            this.graphStorage.save("grafo",graph);
+            return this.mutex.unlock();
+        }).then(() => {
+            if(this.closeWindow === true) window.close();
+        });
+    }
+
+    //colocar em classe separada
+    private getCrawledUrls() : Array<string> {
+        let urlsAnalisadas : string|null = window.localStorage.getItem("urls-analisadas");
+        if(urlsAnalisadas){
+            return JSON.parse(urlsAnalisadas);
+        }
+        else{
+            return [];
+        }
+    }
+    
+    //colocar em classe separada
+    private wasUrlAlreadyCrawled(url : URL) : boolean {
+        const urlsVisitadas : Array<string> = this.getCrawledUrls();
+        for(let urlVisitada of urlsVisitadas){
+            if(this.areURLsEqual(new URL(urlVisitada),url)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //colocar em classe separada
+    private setUrlAsCrawled(url : URL) : void {
+        let urlsAnalisadas : Array<String> = this.getCrawledUrls();
+        urlsAnalisadas.push(url.toString());
+        window.localStorage.setItem("urls-analisadas",JSON.stringify(urlsAnalisadas));
+    }
+    
+    private areURLsEqual(url1 : URL,url2 : URL) : boolean{
+        return url1.toString() == url2.toString();
+    }
+
+    private sameHostname(url1 : URL, url2 : URL) : boolean{
+        return url1.hostname === url2.hostname;
+    }
+
+}
--- src/content.ts
+++ src/content.ts
@@ -1,103 +1,24 @@
 import Mutex from "idb-mutex";
-import { Graph } from "./app/graph/Graph";
 import { GraphStorage } from "./app/graph/GraphStorage";
+import { Crawler } from "./app/Crawler";
 const mu : Mutex = new Mutex('mylock');
 
 let graphStorage = new GraphStorage();
+let crawler : Crawler = new Crawler(graphStorage,mu);
 
 chrome.runtime.sendMessage({ acao: "carregada" });
 
-let fecharJanela : boolean = false;
-
-chrome.runtime.onMessage.addListener(function (request, sender, callback : CallableFunction) {
+chrome.runtime.onMessage.addListener(function (request) {
     const acoes = request.acoes;
     if(acoes){
         if(estaNoArray(acoes,"limpar-grafo")){
             limparGrafo();
         }
         if(estaNoArray(acoes,"analisar")) {
-            
-            const urlPagina : URL = new URL(window.location.href);
-            adicionarNo(urlPagina);
-            const links : HTMLCollectionOf<HTMLAnchorElement> = document.getElementsByTagName('a');
-            for (let i : number = 0; i < links.length; i++) {
-
-                const urlEncontrada : URL = new URL(links[i].href);
-                adicionarNo(urlEncontrada);
-                adicionarLigacao(urlPagina,urlEncontrada);
-                if(urlEhDoMesmoHostname(urlEncontrada) && !urlJaFoiAnalisada(urlEncontrada)){
-                    adicionarUrlAnalisada(new URL(links[i].href));
-                    abrirJanela(links[i].href);
-                }
-            }
-            callback();
-            fecharJanela = true;
+            crawler.crawl();
         }
     }
 });
-
-function adicionarUrlAnalisada(url : URL) : void {
-    let urlsAnalisadas : Array<String> = obterUrlsAnalisadas();
-    urlsAnalisadas.push(url.toString());
-    set("urls-analisadas",JSON.stringify(urlsAnalisadas));
-}
-
-function get(key : string) : string|null{
-    return window.localStorage.getItem(key);
-}
-
-function set(key : string,value : string) : void{
-    window.localStorage.setItem(key,value);
-}
-
-function obterUrlsAnalisadas() : Array<string>{
-    let urlsAnalisadas : string|null = get("urls-analisadas");
-    if(urlsAnalisadas){
-        return JSON.parse(urlsAnalisadas);
-    }
-    else{
-        return [];
-    }
-}
-
-function urlJaFoiAnalisada(url : URL) : boolean{
-    const urlsVisitadas : Array<string> = obterUrlsAnalisadas();
-    for(let urlVisitada of urlsVisitadas){
-        if(urlsSaoIguais(new URL(urlVisitada),url)){
-            return true;
-        }
-    }
-    return false;
-}
-
-function urlsSaoIguais(url1 : URL,url2 : URL) : boolean{
-    return url1.toString() == url2.toString();
-}
-
-//FUNCOES RELACIONADAS A GRAFO
-
-function adicionarNo(url : URL) : void{
-    mu.lock().then(() => {
-        var grafo = graphStorage.get("grafo");
-        grafo.addNode(url.toString());
-        graphStorage.save("grafo",grafo);
-        return mu.unlock();
-    }).then(() => {
-        console.log(fecharJanela);
-        if(fecharJanela === true) window.close();
-    });
-}
-
-function adicionarLigacao(url1 : URL,url2 : URL) : void{
-    mu.lock().then(() => {
-        const grafo : Graph = graphStorage.get("grafo");
-        grafo.addEdge(url1.toString(),url2.toString());
-        graphStorage.save("grafo",grafo);
-        return mu.unlock();
-    }).then(() => {
-        if(fecharJanela === true) window.close();
-    });
-}
 
 function limparGrafo() : void{
     graphStorage.remove("grafo");
@@ -107,16 +28,7 @@ function limparGrafo() : void{
 
 // OUTRAS FUNÇÕES
 
-function abrirJanela(url : string) : void {
-    chrome.runtime.sendMessage({ acao: "abrir-janela", url: url });
-}
-
 function estaNoArray(array : Array<any> ,valor : any) : boolean{
     if(array.indexOf(valor) !== -1) return true;
     return false;
-}
-
-function urlEhDoMesmoHostname(urlASerVisitada : URL) : boolean{
-    const urlDaPagina : URL = new URL(window.location.href);
-    return urlDaPagina.hostname === urlASerVisitada.hostname;
 }
