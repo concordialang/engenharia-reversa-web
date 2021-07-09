5d1a7e7b7e19efa2eb6d596f4c5cedfeb309bc9b
Matheus Cristani <matheus.cristani27@gmail.com>
2021-07-08 23:55:02 -0300

    Adiciona testes para sentenças de mutations

--- src/spec-analyser/VariantSentencesGenerator.ts
+++ src/spec-analyser/VariantSentencesGenerator.ts
@@ -41,42 +41,65 @@ export class VariantSentencesGenerator {
 		for (let mutation of mutations) {
 			if (mutation.type === 'attributes') {
 				if (mutation.attributeName === 'style') {
-					if (mutation.oldValue !== null) {
-						let oldValueArray = mutation.oldValue.split(':');
-						let attribute = oldValueArray[0];
-						let oldValue = oldValueArray[1].replace(';', '').replace(' ', '');
-
-						if (attribute === 'display') {
-							if (oldValue === 'none' && mutation.target.style.display === 'block') {
-								console.log('E eu vejo o elemento {#' + mutation.target.id + '}');
-								//gerar nome para mutacao
-								sentences.push(
-									new VariantSentence(
-										VariantSentenceType.WHEN,
-										VariantSentenceActions.SEE,
-										['{' + mutation.target.id + '}']
-									)
-								);
-							}
-
-							if (oldValue === 'block' && mutation.target.style.display === 'none') {
-								console.log(
-									'E eu não vejo o elemento {#' + mutation.target.id + '}'
-								);
-								sentences.push(
-									new VariantSentence(
-										VariantSentenceType.WHEN,
-										VariantSentenceActions.NOTSEE,
-										['{' + mutation.target.id + '}']
-									)
-								);
-							}
-						}
-					}
+					this.buildStyleSentence(sentences, mutation);
 				}
 			}
 		}
 
 		return sentences;
+	}
+
+	private buildStyleSentence(sentences, mutation) {
+		let oldValueArray;
+		let attribute;
+		let oldValue;
+
+		if (mutation.oldValue !== null) {
+			oldValueArray = mutation.oldValue.split(':');
+			attribute = oldValueArray[0];
+			oldValue = oldValueArray[1].replace(';', '').replace(' ', '');
+		} else {
+			let attributesElm = Object.assign({}, mutation.target.attributes);
+
+			if (!attributesElm) {
+				return;
+			}
+
+			let styleValue: string = '';
+			for (const attr of Object.values(attributesElm)) {
+				if (attr instanceof Attr && attr.nodeName === 'style') {
+					styleValue = attr.value;
+				}
+			}
+
+			if (styleValue == '') {
+				return;
+			}
+
+			oldValueArray = styleValue.split(':');
+			attribute = oldValueArray[0];
+			oldValue = oldValueArray[1].replace(';', '').replace(' ', '');
+		}
+
+		if (attribute === 'display') {
+			if (oldValue === 'none' && mutation.target.style.display === 'block') {
+				console.log('E eu vejo o elemento {#' + mutation.target.id + '}');
+				//gerar nome para mutacao
+				sentences.push(
+					new VariantSentence(VariantSentenceType.WHEN, VariantSentenceActions.SEE, [
+						'{' + mutation.target.id + '}',
+					])
+				);
+			}
+
+			if (oldValue === 'block' && mutation.target.style.display === 'none') {
+				console.log('E eu não vejo o elemento {#' + mutation.target.id + '}');
+				sentences.push(
+					new VariantSentence(VariantSentenceType.WHEN, VariantSentenceActions.NOTSEE, [
+						'{' + mutation.target.id + '}',
+					])
+				);
+			}
+		}
 	}
 }
